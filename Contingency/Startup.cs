79ab3ac4cde7038ec44d8a8d957d14535e4b77c8
mbrannen747 <mbrannen@student.neumont.edu>
2017-11-06 19:06:40 -0700

    Cookie

---
+++ Contingency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Contingency.Startup))]
+namespace Contingency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
