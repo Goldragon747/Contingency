82aaf1e107798aaaaf079ef3d8b20efdc67f56c9
mbrannen747 <mbrannen@student.neumont.edu>
2017-11-10 16:04:09 -0700

    Tequila

---
+++ Contingency/Controllers/DungeonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contingency.Controllers
+{
+    public class DungeonController : Controller
+    {
+        // GET: Dungeon
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
