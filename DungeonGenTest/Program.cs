414cacde0df431d43af265ed199fe65f0478c1e6
SamKnow <sknowlton@student.neumont.edu>
2017-11-14 15:04:36 -0700

    A temp console app for testing dungeon generation

---
+++ DungeonGenTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using DungeonLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGenTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dungeon d = new Dungeon("test", 20, 20, 0);
+            Console.WriteLine(d.ToString());
+        }
+    }
+}
