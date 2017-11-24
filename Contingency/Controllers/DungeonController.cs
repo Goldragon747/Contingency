9661405de9bf3f7f570d98d67cbdea0104b5e24e
mbrannen747 <mbrannen@student.neumont.edu>
2017-11-24 16:32:19 -0700

    Scrambled Eggs
    Changes:
    - Build some of the footwork for the DM page with a class(currently empty, but will contain EVERYTHING about the dungeon, including loot and monsters) being passed around.

--- Contingency/Controllers/DungeonController.cs
+++ Contingency/Controllers/DungeonController.cs
@@ -1,4 +1,5 @@
-﻿using DungeonLib.Model;
+﻿using Contingency.Models;
+using DungeonLib.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,37 +11,21 @@ namespace Contingency.Controllers
     public class DungeonController : Controller
     {
         // GET: Dungeon
+        [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            DungeonCreator dc = new DungeonCreator();
+            return View(dc);
         }
-        public ActionResult DM()
+        [HttpPost]
+        public ActionResult Create(DungeonCreator dc)
         {
-            int[,] hardcode =
-            {
-                { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-                { 0,0,0,2,2,2,0,0,0,0,0,0,5,5,5,0,0,0,0,0 },
-                { 1,1,1,2,2,2,0,4,4,4,0,0,5,5,5,0,0,0,0,0 },
-                { 0,0,0,2,2,2,0,4,4,4,1,1,5,5,5,0,0,0,0,0 },
-                { 0,0,0,0,1,0,0,4,4,4,0,0,5,5,5,0,0,0,0,0 },
-                { 0,0,0,0,1,0,0,0,1,0,0,0,5,5,5,0,0,0,0,0 },
-                { 0,0,3,3,3,3,3,0,1,0,0,0,0,1,0,0,0,0,0,0 },
-                { 0,0,3,3,3,3,3,0,1,0,0,0,0,1,0,0,0,0,0,0 },
-                { 0,0,3,3,3,3,3,1,1,0,0,0,1,1,0,0,0,0,0,0 },
-                { 0,0,3,3,3,3,3,0,0,0,0,0,1,0,0,0,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,6,6,6,6,6,6,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,6,6,6,6,6,6,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,6,6,6,6,6,6,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,6,6,6,6,6,6,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
-            };
-            Dungeon d = new Dungeon("Default Dungeon",hardcode);
-            return View(d);
+            return RedirectToAction("DM", dc);
         }
+        public ActionResult DM(DungeonCreator dc)
+        {
+            return View(dc);
+        }
+        
     }
 }
