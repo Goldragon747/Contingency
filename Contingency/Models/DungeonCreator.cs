fd69fcce93d911bbc9255a6bf7fa0076e0d51301
Gabe Barber <GBarber@student.neumont.edu>
2017-11-29 15:51:13 -0700

    Fixed The Broken World

--- Contingency/Models/DungeonCreator.cs
+++ Contingency/Models/DungeonCreator.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using static Contingency.Models.Enums;
 
 namespace Contingency.Models
 {
     public class DungeonCreator
     {
+        public DungeonSize DS { get; set; }
+        public EnemyGroups EG { get; set; }
+        public LootGroups LG { get; set; }
+        public CRPresets CR { get; set; }
+        public bool Traps { get; set; }
+        public int LootByAmount { get; set; }
+        public int MinValue { get; set; }
     }
 }
